577f98915519c8a965338d9e84620c0de2edc4d7
Jeff Ashton <Jeff.Ashton@D2L.com>
2018-01-31 21:02:38 -0500

    Committing reference implementation

---
+++ AwsIotMqttWebSocketSigner.Tests/SigV4UtilsTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net;
+using Amazon.Runtime;
+using NUnit.Framework;
+
+namespace AwsIotMqttWebSocketSigner.Tests {
+
+	[TestFixture]
+	public sealed class SigV4UtilsTests {
+
+		[Test]
+		public void GetSignedUri() {
+
+			DateTime now = DateTime.Parse( "2018-02-01T01:34:16Z" );
+
+			string url = SigV4Utils.GetSignedUri(
+				host: "hhhhhhhhhhhhh.iot.us-east-1.amazonaws.com",
+				region: "us-east-1",
+				credentials: new ImmutableCredentials(
+					awsAccessKeyId: "AAAAAAAAAA/AAAAAAAAA",
+					awsSecretAccessKey: "KKKKKKKKKKKKKKKKKKK/KKKKKKKKKKKKKKKKKKKK",
+					token: "FQoDYXdzECIaDPbKjb+bjsordZDleyKyAqJkCdi62dXvhNApoGILpqj0IV1zqKwxgRApCX7dOY2078lk/1KSSOEOiiFy997CuhohkS0tsatZ5hHoxu/gHQlZWB3yLTgKrdhtXWAIuPRyomSxjr81jAi6VDN0wGJIsncISpDzKbvYlgVPhlME3k7J+eS7ozprgSrEgeA8Kuqjuo/OxFzdwe2P1ptbOGF1DMk4Wa89aUUD07Of7pQUUQENI2W+y6Ik/jZLxbiYp5aY7yTiN0PI25ej4yYIZ3Rfh2b+91ZwaAN6ZNBzggbtBsW55qs0xYsyiWT0ovIXJag2rqmAq3PMlAbGj7Ss4Wweh1zXqm23SeKuiIZ7bpeM8jKtOdY4Y0L9DUCojXU997niVDC34yWFLwYGPyGSN8iUOUCetRla+9A80luWZl6d+VGHqijxyMnTBQ=="
+				),
+				now: now
+			);
+
+			Assert.AreEqual(
+				"wss://hhhhhhhhhhhhh.iot.us-east-1.amazonaws.com/mqtt?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AAAAAAAAAA%2FAAAAAAAAA%2F20180201%2Fus-east-1%2Fiotdevicegateway%2Faws4_request&X-Amz-Date=20180201T013416Z&X-Amz-SignedHeaders=host&X-Amz-Signature=001ce951660ebfc126c535407643aac7a183600e54e6923194e38225931461e8&X-Amz-Security-Token=FQoDYXdzECIaDPbKjb%2BbjsordZDleyKyAqJkCdi62dXvhNApoGILpqj0IV1zqKwxgRApCX7dOY2078lk%2F1KSSOEOiiFy997CuhohkS0tsatZ5hHoxu%2FgHQlZWB3yLTgKrdhtXWAIuPRyomSxjr81jAi6VDN0wGJIsncISpDzKbvYlgVPhlME3k7J%2BeS7ozprgSrEgeA8Kuqjuo%2FOxFzdwe2P1ptbOGF1DMk4Wa89aUUD07Of7pQUUQENI2W%2By6Ik%2FjZLxbiYp5aY7yTiN0PI25ej4yYIZ3Rfh2b%2B91ZwaAN6ZNBzggbtBsW55qs0xYsyiWT0ovIXJag2rqmAq3PMlAbGj7Ss4Wweh1zXqm23SeKuiIZ7bpeM8jKtOdY4Y0L9DUCojXU997niVDC34yWFLwYGPyGSN8iUOUCetRla%2B9A80luWZl6d%2BVGHqijxyMnTBQ%3D%3D",
+				url
+			);
+		}
+	}
+}
---
+++ AwsIotMqttWebSocketSigner/SigV4Utils.cs
@@ -0,0 +1,117 @@
+﻿/***************************************************************************************
+*    Title: AWS IoT MQTT Over the WebSocket Protocol
+*    Author: Amazon Web Services, Inc.
+*    Date: 2018-01-31
+*    Availability: https://docs.aws.amazon.com/iot/latest/developerguide/protocols.html
+*
+***************************************************************************************/
+
+using System;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+using Amazon.Runtime;
+
+namespace AwsIotMqttWebSocketSigner {
+
+	public static class SigV4Utils {
+
+		private static readonly UTF8Encoding m_encoding = new UTF8Encoding( false );
+
+		private static string AsHex( this byte[] bytes ) {
+
+			StringBuilder sb = new StringBuilder( bytes.Length * 2 );
+			for( int i = 0; i < bytes.Length; i++ ) {
+				sb.AppendFormat( "{0:x2}", bytes[ i ] );
+			}
+			return sb.ToString();
+		}
+
+		private static string GetSha256( string data ) {
+
+			using( SHA256 algo = SHA256.Create() ) {
+				byte[] bytes = m_encoding.GetBytes( data );
+				byte[] hash = algo.ComputeHash( bytes );
+				return hash.AsHex();
+			}
+		}
+
+		private static byte[] GetHmac( string key, string data ) {
+
+			byte[] keyBytes = m_encoding.GetBytes( key );
+			return GetHmac( keyBytes, data );
+		}
+
+		private static byte[] GetHmac( byte[] key, string data ) {
+
+			using( HMACSHA256 kha = new HMACSHA256( key ) ) {
+				byte[] dataBytes = m_encoding.GetBytes( data );
+				byte[] hash = kha.ComputeHash( dataBytes );
+				return hash;
+			}
+		}
+
+		private static byte[] GetSignatureKey( string key, string date, string region, string service ) {
+
+			byte[] kDate = GetHmac( "AWS4" + key, date );
+			byte[] kRegion = GetHmac( kDate, region );
+			byte[] kService = GetHmac( kRegion, service );
+			byte[] kCredentials = GetHmac( kService, "aws4_request" );
+			return kCredentials;
+		}
+
+		public static string GetSignedUri(
+					string host,
+					string region,
+					ImmutableCredentials credentials
+				) {
+
+			return GetSignedUri(
+				host: host,
+				region: region,
+				credentials: credentials,
+				now: DateTime.UtcNow
+			);
+		}
+
+		internal static string GetSignedUri(
+				string host,
+				string region,
+				ImmutableCredentials credentials,
+				DateTime now
+			) {
+
+			DateTime utcNow = now.ToUniversalTime();
+			string datetime = utcNow.ToString( "yyyyMMddTHHmmssZ" );
+			string date = datetime.Substring( 0, 8 );
+
+			const string method = "GET";
+			const string protocol = "wss";
+			const string uri = "/mqtt";
+			const string service = "iotdevicegateway";
+			const string algorithm = "AWS4-HMAC-SHA256";
+
+			string credentialScope = date + "/" + region + "/" + service + "/" + "aws4_request";
+			string canonicalQuerystring = "X-Amz-Algorithm=" + algorithm;
+			canonicalQuerystring += "&X-Amz-Credential=" + WebUtility.UrlEncode( credentials.AccessKey + "/" + credentialScope );
+			canonicalQuerystring += "&X-Amz-Date=" + datetime;
+			canonicalQuerystring += "&X-Amz-SignedHeaders=host";
+
+			string canonicalHeaders = "host:" + host + "\n";
+			string payloadHash = GetSha256( "" );
+			string canonicalRequest = method + "\n" + uri + "\n" + canonicalQuerystring + "\n" + canonicalHeaders + "\nhost\n" + payloadHash;
+
+			string stringToSign = algorithm + "\n" + datetime + "\n" + credentialScope + "\n" + GetSha256( canonicalRequest );
+			byte[] signingKey = GetSignatureKey( credentials.SecretKey, date, region, service );
+			string signature = GetHmac( signingKey, stringToSign ).AsHex();
+
+			canonicalQuerystring += "&X-Amz-Signature=" + signature;
+			if( !String.IsNullOrEmpty( credentials.Token ) ) {
+				canonicalQuerystring += "&X-Amz-Security-Token=" + WebUtility.UrlEncode( credentials.Token );
+			}
+
+			string requestUrl = protocol + "://" + host + uri + "?" + canonicalQuerystring;
+			return requestUrl;
+		}
+	}
+}
